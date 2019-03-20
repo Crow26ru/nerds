a30173a94889ee96f140ef0ee1a40a02256d750e
Семен <7tacson7@gmail.com>
2019-03-20 13:20:24 +0300

    Заинлайнены недостающие SVG

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,35 @@ var plumber = require('gulp-plumber');
 var postcss = require('gulp-postcss');
 var autoprefixer = require('autoprefixer');
 var server = require('browser-sync').create();
+var rename = require('gulp-rename');
+var svgstore = require('gulp-svgstore');
+var image = require('gulp-image');
+
+gulp.task('image', function () {
+  return gulp.src('source/img/**/*')
+    .pipe(image({
+      pngquant: true,
+      optipng: false,
+      zopflipng: false,
+      jpegRecompress: false,
+      mozjpeg: true,
+      guetzli: false,
+      gifsicle: false,
+      svgo: true,
+      concurrent: 10,
+      quiet: true
+    }))
+    .pipe(gulp.dest('dest/img'));
+});
+
+gulp.task('sprite', function () {
+  return gulp.src('source/img/*-icon.svg')
+    .pipe(svgstore({
+      inlineSvg: true
+    }))
+    .pipe(rename('sprite.svg'))
+    .pipe(gulp.dest('dest/img'));
+});
 
 gulp.task('css', function () {
   return gulp.src('source/less/style.less')
@@ -29,6 +58,7 @@ gulp.task("server", function () {
 
   gulp.watch("source/less/**/*.less", gulp.series("css"));
   gulp.watch("source/*.html").on("change", server.reload);
+  gulp.watch("source/js/*.js").on("change", server.reload);
 });
 
 gulp.task("start", gulp.series("css", "server"));
